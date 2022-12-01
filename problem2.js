572caf1d40d85b1d920a39e1598ddf42e690fa70
Malashri Hiremath <malashrihiremath1099@gmail.com>
2022-12-02 00:21:37 +0530

    Done

---
+++ problem2.js
@@ -0,0 +1,14 @@
+let str="naman"
+
+let reverse= "";
+ 
+ for(let i=num.length-1 ; i>=0; i--){
+   reverse=reverse+str[i];
+}
+
+ if(reverse==num){
+ console.log("Yes");
+   }
+  else{
+ console.log("No");
+  }
